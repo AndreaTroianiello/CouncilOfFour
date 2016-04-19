d790ecb366b9749f22c8edaac53b2264eb12b240
doppiavi <vincenzovisco94@gmail.com>
2016-04-19 15:14:49 +0200

    modificato app.java

--- cg23/src/main/java/it/polimi/ingsw/cg23/App.java
+++ cg23/src/main/java/it/polimi/ingsw/cg23/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!, sei sicuro?" );
+        System.out.println( "Hello World!, sei sicuro? Non tanto" );
     }
 }
