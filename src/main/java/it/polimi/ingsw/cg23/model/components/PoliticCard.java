ceb7156bda3f205081ddb9eefbf1dc1c32ed07b5
Andrea <andrea.troianiello.94@gmail.com>
2016-04-28 15:42:12 +0200

    Aggiunti i primi componenti, assieme al giocatore.

---
+++ src/main/java/it/polimi/ingsw/cg23/model/components/PoliticCard.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.cg23.model.components;
+
+public class PoliticCard {
+	private final String color;
+
+	public PoliticCard(String color) {
+		this.color = color;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+	
+	
+}
