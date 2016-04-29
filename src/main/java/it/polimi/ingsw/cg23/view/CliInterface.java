6161887c48de8bf46c7e08ce432b8b510ea6eae5
viga94 <viga94_@hotmail.it>
2016-04-29 12:51:11 +0200

    modificata la view

--- src/main/java/it/polimi/ingsw/cg23/view/CliInterface.java
+++ src/main/java/it/polimi/ingsw/cg23/view/CliInterface.java
@@ -1,6 +1,13 @@
 package it.polimi.ingsw.cg23.view;
 
-public class CliInterface {
-//classe per la comunicazione con l'utente
+import java.util.Scanner;
 
+public class CliInterface {//classe per la comunicazione con l'utente
+
+	Scanner scan=new Scanner(System.in);
+	ReadXml newpartita=new ReadXml();
+	public void StartPartita(int players){//inizializza il file per la partita
+		String lettura=newpartita.ReadFile();
+	}
+	
 }
