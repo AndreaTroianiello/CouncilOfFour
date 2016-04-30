ff027f16e86c532253ed9ef9ed7a06b076d0ef73
Andrea <andrea.troianiello.94@gmail.com>
2016-04-30 16:26:48 +0200

    Implementate parzialmente le regioni e le città

--- src/main/java/it/polimi/ingsw/cg23/model/City.java
+++ src/main/java/it/polimi/ingsw/cg23/model/City.java
@@ -1,5 +1,92 @@
 package it.polimi.ingsw.cg23.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import it.polimi.ingsw.cg23.model.bonus.Bonus;
+
 public class City {
-	//to do
+	private final char id;
+	private final String name;
+	private final Bonus bonus;
+	private final String type;
+	private final Region region;
+	private final List<City> neighbors;
+	private boolean bonusAvailable;
+	
+	public City(char id, String name, Bonus bonus, String type, Region region){
+		this.id=id;
+		this.name=name;		
+		this.bonus=bonus;
+		this.type=type;
+		this.region=region;
+		this.neighbors=new ArrayList();
+		this.bonusAvailable=true;
+	}
+	
+	/**
+	 * @return get the id of city
+	 */
+	public char getId() {
+		return id;
+	}
+
+	/**
+	 * @return get the name of city
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return get the type of city 
+	 */
+	public String getType() {
+		return type;
+	}
+	
+	/**
+	 * @return get the region of city
+	 */
+	public Region getRegion() {
+		return region;
+	}
+	
+
+	/**
+	 * 
+	 * @param neighbor add a city at neighbors
+	 */
+	public void addNeighbor(City neighbor){
+		neighbors.add(neighbor);
+	}
+	
+	/**
+	 * @return all neighbors of the city
+	 */
+	public List<City> getNeighbors() {
+		return neighbors;
+	}
+	
+	/**
+	 * @return see if bonus is usable
+	 */
+	public boolean isBonusAvailable() {
+		return bonusAvailable;
+	}
+
+	/**
+	 *  set bonusAvailable at false
+	 */
+	public void setBonusUnavailable() {
+		this.bonusAvailable = false;
+	}
+
+	/**
+	 *  run the bonus associated to the city
+	 */
+	public void runBonus(Player player){
+		if(bonusAvailable)
+			bonus.esegui(player);
+	}
+	
 }
---
+++ src/main/java/it/polimi/ingsw/cg23/model/Region.java
@@ -0,0 +1,64 @@
+package it.polimi.ingsw.cg23.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import it.polimi.ingsw.cg23.model.bonus.Bonus;
+
+public class Region {
+
+	private final String name;
+	private final Bonus bonus;
+	private final List<City> cities;
+	private boolean bonusAvailable;
+	
+	public Region(String name, Bonus bonus) {
+		this.name = name;
+		this.bonus = bonus;
+		this.cities = new ArrayList();
+		this.bonusAvailable = true;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the bonusAvailable
+	 */
+	public boolean isBonusAvailable() {
+		return bonusAvailable;
+	}
+
+	/**
+	 *  set bonusAvailable at false
+	 */
+	public void setBonusUnavailable() {
+		this.bonusAvailable = false;
+	}
+
+	/**
+	 * @return the cities
+	 */
+	public List<City> getCities() {
+		return cities;
+	}
+	
+	/**
+	 * @param the city put the city in the region
+	 */
+	public void addCity(City city){
+		cities.add(city);
+	}
+	
+	/**
+	 *  run the bonus associated to the region
+	 */
+	public void runBonus(Player player){
+		if(bonusAvailable)
+			bonus.esegui(player);
+	}
+
+}
