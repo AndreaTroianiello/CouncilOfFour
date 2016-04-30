9d76a7de9e864e532963a33d0e2a19cf4c179d7e
viga94 <viga94_@hotmail.it>
2016-04-30 19:04:02 +0200

    modificato readxml

--- src/main/java/it/polimi/ingsw/cg23/view/CliInterface.java
+++ src/main/java/it/polimi/ingsw/cg23/view/CliInterface.java
@@ -5,15 +5,27 @@ package it.polimi.ingsw.cg23.view;
 public class CliInterface {//classe per la comunicazione con l'utente
 	//per main
 	//CliInterface nuovo=new CliInterface();
-	//nuovo.StartPartita(3);
+	//nuovo.StartPartita(4);
 
 	//Scanner scan=new Scanner(System.in);
 	ReadXml newpartita=new ReadXml();
-	int citynum=15;//numero di citta'
-	String[][] city=new String[citynum][3];//array multidim con city name, link e color
+	final int citynum=15;//numero di citta'
+	String[][] city=new String[citynum][4];//array multidim con city name, link e color, zone
+	
+	public void prova(){//PROVA PER ME
+		
+		String[][] city=new String[citynum][4];
+		for(int i=0;i<city.length;i++){
+			for(int k=0;k<4;k++){
+				city[i][k]=Integer.toString(i);
+			}
+		}
+		//String[][] city={[][][][]};
+		CreateMap(city);
+	}
 	
 	public void StartPartita(int players){//carica il file con le infromazioni della partita
-		String lettura=newpartita.ReadFileXml("cities");
+		String lettura=newpartita.ReadFileXml();
 		System.out.println(lettura);
 	}
 	
@@ -21,18 +33,22 @@ public class CliInterface {//classe per la comunicazione con l'utente
 		System.out.println(testo+" "+ogg);
 	}
 	
-	public void CreateMap(int del, String[][] city){
-		String plancia=null;//la stringa che stampa la plancia di gioco
-		/*for(int i=0; i<city.length; i++){
-			for(int k=0; k<3; k++){
+	public void CreateMap(String[][] city){//stampa la mappa
+		/*String plancia="   costa         collina          montagna\n";//la stringa che stampa la plancia di gioco
+		//System.out.println(city.length);
+		int n=city.length;
+		for(int i=0; i<n; i++){//le righe da stampare sono 3
+			//for(int k=0; k<3; k++){
+				plancia+=city[i][0]+"("+city[i][1]+") ";//0: nome citta', 1: colore citta'
+				if(i<=9)plancia+="  ";//aggiunge piu' spazi se 
+			//}
+				if((i+1)%6==0)plancia+="\n";
+				//if()
 				
-				if(k==0)System.out.println(city[i][0]);
-				if(k==1){
-					System.out.println("--"+city[i][1]+"\n");
-					System.out.println("--"+city[i][1]+"\n");
-				}
-			}
-		}*/
+		}
+		
+		System.out.println(plancia);
+	*/	
 	}
-	
+		
 }
--- src/main/java/it/polimi/ingsw/cg23/view/ReadXml.java
+++ src/main/java/it/polimi/ingsw/cg23/view/ReadXml.java
@@ -10,72 +10,70 @@ import org.w3c.dom.Element;
 
 public class ReadXml {//http://www.tutorialspoint.com/java_xml/java_dom_parse_document.htm
 	//http://www.mrwebmaster.it/java/xml-java-esempio-parsing-jaxp_7488_2.html
-	
+
 	/*
 	 * legge il file xml
 	 */
-	public String ReadFileXml(String nodo){
+	public String ReadFileXml(){
 		try {	
 			File inputFile = new File("src/main/java/it/polimi/ingsw/cg23/view/ConfigurazionePartita.xml");
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();//creata la factory per processare il flusso di dati
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();//inizializzato un nuovo documento
 			Document doc = dBuilder.parse(inputFile);//carica il documento dal file
-			Node rootnode = doc.getFirstChild();//recupera il primo nodo dell'xml
 
+			Node rootnode = doc.getFirstChild();//recupera il primo nodo dell'xml (map)
+
+			int citynum=15;//PROVVISORIO -> DA RECUPERARE DA XML
+			System.out.println(rootnode.getNodeName());
+			String[][] city=new String[citynum][4];//si salvano le infromazioni delle citta'
 			/////////////OK
-			if(rootnode.getNodeName()==nodo){//il nodo di root e' quello cercato
-				return rootnode.getTextContent();
-			}else{
-				for(int i=0; i<rootnode.getChildNodes().getLength(); i++){//cerco i nodi figli del nodo root (map)
-					Node zonenode = rootnode.getChildNodes().item(i);//il secondo nodo di xml e' zone
-					if(zonenode.getNodeName()==nodo){//il secondo nodo è quello cercato
-						if(zonenode.hasAttributes()==true)
-						return rootnode.getTextContent();
-					}else{
-						for(int j=0; i<zonenode.getChildNodes().getLength(); j++){//cerco i nodi figli del nodo zone
-							Node cities = zonenode.getChildNodes().item(j);//il terzo nodo di xml e' cities
-							if(cities.getNodeName()==nodo){//il terzo nodo è quello cercato
-								return cities.getTextContent();
-							}else{
-								for(int k=0; i<cities.getChildNodes().getLength(); k++){//cerco i nodi figlio del nodo cities
-									Node city = cities.getChildNodes().item(k);//il quarto nodo di xml e' city
-									/*if(zonenode.getNodeName()==nodo){//il quarto nodo è quello cercato
-										//System.out.println("okno");
-										return cities.getTextContent();
-										
-									}*/
-								}
+			
+			//Node zone = rootnode.getChildNodes().item(1);//secondo nodo xml (zone)
+			//Node zonename= zone.getChildNodes().item(1);//attributo name di costa
+			//Node cities= zone.getChildNodes().item(3);//quarto nodo xml (cities)
+			
+			NodeList lista=doc.getElementsByTagName("city");
+			
+			
+			
+			// Iteriamo per ogni nodo presente nella lista dei nodi della radice
+			for (int i = 0; i < lista.getLength(); i++) {
+				Node actual=lista.item(i);
+				Element ele=(Element) actual;
+				
+				//System.out.println(ele.getElementsByTagName("name").item(0).getTextContent());
+				
+				city[i][0]=ele.getElementsByTagName("name").item(0).getTextContent();
+				
+			}
+				// Sapendo che il primo nodo è il nodo libro procediamo iterando nei suoi nodi figli
+				
+				
+				//Se il nodo è un Elemento allora ne stampiamo il nome, il testo contenuto e gli attributi se presenti
+				/*for (int j = 0; j < zone.getChildNodes().getLength(); j++) {
+					Node cities = zone.getChildNodes().item(j);
+					for(int m=0; m<cities.getChildNodes().getLength(); m++){
+						Node citty = cities.getChildNodes().item(m);
+					
+					if (element.getNodeType() == Node.ELEMENT_NODE) {
+						System.out.println("Nome dell'elemento: " + element.getNodeName() + " - Valore del testo = " + element.getTextContent());
+						if (element.hasAttributes()) {
+							for (int k = 0; k < element.getAttributes().getLength(); k++) {
+								Node attribute = element.getAttributes().item(k);
+								System.out.println("\tNome dell'attributo: " + attribute.getNodeName() + " - Valore del testo = " + attribute.getTextContent());
 							}
 						}
 					}
+					}
+				}*/
+			
+			
+			for(int i=0;i<city.length;i++){
+				for(int k=0;k<4;k++){
+					System.out.print(city[i][k]+" ");
 				}
+				System.out.println("\n");
 			}
-			//System.out.println(rootnode.getNodeName());
-			// Iteriamo per ogni nodo presente nella lista dei nodi della radice
-			/*for (int i = 0; i < rootnode.getChildNodes().getLength(); i++) {
-
-				// Sapendo che il primo nodo è il nodo zona procediamo iterando nei suoi nodi figli
-				Node zonenode = rootnode.getChildNodes().item(i);
-				String zoneAttribute=null;
-				/*if(zonenode.hasAttributes()){
-		            	 zoneAttribute=zonenode.getTextContent();
-		             }
-				System.out.println("1"+zonenode.getNodeName()+"1");
-				for (int j = 0; j < zonenode.getChildNodes().getLength(); j++) {
-
-					Node element = zonenode.getChildNodes().item(j);
-					/* if (element.getNodeType() == Node.ELEMENT_NODE) {
-		                     System.out.println("Nome: " + element.getNodeName() + " - Valore: " + element.getTextContent());
-		                   /*  if (element.hasAttributes()) {
-		                         for (int k = 0; k < element.getAttributes().getLength(); k++) {
-		                             Node attribute = element.getAttributes().item(k);
-		                             System.out.println("\tNome dell'attributo: " + attribute.getNodeName() + " - Valore del testo = " + attribute.getTextContent());
-		                         }
-		                     }
-					//}
-				}
-			}*/
-
 			return "ok";
 		} catch (Exception e) {
 			// e.printStackTrace();
