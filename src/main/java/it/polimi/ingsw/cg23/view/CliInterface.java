b624da6ddcb6e483ce22db9911fc1ebed84e2ff9
viga94 <viga94_@hotmail.it>
2016-05-01 11:45:45 +0200

    stampa la mappa parizialmente (con errori)

--- src/main/java/it/polimi/ingsw/cg23/view/CliInterface.java
+++ src/main/java/it/polimi/ingsw/cg23/view/CliInterface.java
@@ -6,23 +6,25 @@ package it.polimi.ingsw.cg23.view;
 public class CliInterface {
 	//per main
 	//CliInterface nuovo=new CliInterface();
-	//nuovo.StartPartita(4);
+	//nuovo.startPartita();
 
 	ReadXml lettureXml=new ReadXml();
 	final int citynum=lettureXml.cityNumber();//numero di citta'
 	final int cityNodeNumber=lettureXml.cityNodeNumber();//numero di attributi delle citta'
+
 	String[][] cityInfo=new String[citynum][cityNodeNumber];//array multidim con city name, link, color, zone
-	
+
 	/**
 	 * carica il file con le infromazioni della partita
 	 * @return void
 	 * @param number of players
 	 */
 	public void startPartita(){
+		//System.out.print(cityNodeNumber);
 		cityInfo=lettureXml.readFileXml();
-		printArray(cityInfo);
+		createMap(cityInfo);//stampa array solo per le prove
 	}
-	
+
 	/**
 	 * stampa un array bidinemsionale
 	 * @return void
@@ -33,9 +35,10 @@ public class CliInterface {
 			for(int k=0; k<array[0].length; k++){
 				System.out.print(array[i][k]+"    ");
 			}
+			System.out.print("\n");
 		}
 	}
-	
+
 	/**
 	 * stampa una qualunque cosa gli viene passata
 	 * @return void
@@ -45,28 +48,46 @@ public class CliInterface {
 	public void print(Object ogg, String testo){
 		System.out.println(testo+" "+ogg);
 	}
-	
+
 	/**
-	 * stampa la mappa (non funziona)
+	 * stampa la mappa ( funziona parzialmente)
 	 * @return void
 	 * @param bidimensional array with city
 	 */
 	public void createMap(String[][] city){
-		/*String plancia="   costa         collina          montagna\n";//la stringa che stampa la plancia di gioco
-		System.out.println(city.length);
-		int n=city.length;
-		for(int i=0; i<n; i++){//le righe da stampare sono 3
-			for(int k=0; k<3; k++){
-				plancia+=city[i][0]+"("+city[i][1]+") ";//0: nome citta', 1: colore citta'
-				if(i<=9)plancia+="  ";//aggiunge piu' spazi se 
-			}
-				if((i+1)%6==0)plancia+="\n";
-				if()
+		String plancia="     Costa             Collina              Montagna\n";//la stringa che stampa la plancia di gioco
+		int i=0, c=city.length/3, m=city.length/3*2;
+		for(i=0; i<city.length/3;){//le righe da stampare sono 5
+			//System.out.println("II"+i);
+			for(int k=0; k<3; k++){//aggiunge le 3 citta', una per ogni regione
+				int kk=k;
+				if(k==0)k=i;
+				if(k==1)k=c;
+				if(k==2)k=m;
+				
+				String newcity=city[k][0]+"("+city[k][1]+") ";
 
+					//CONTROLLARE
+				int length=20-newcity.length();
+				if(length<20){
+					for(int j=0; j<length; j++){
+						newcity+=" ";
+						//System.out.println(add+"|");
+					}
+				}
+				plancia+=newcity;
+				k=kk;
+			}
+			i++;
+			c++;
+			System.out.println(i);
+			m++;
+			System.out.println(m);
+			plancia+="\n";
 		}
 
 		System.out.println(plancia);
-		 */	
+
 	}
 
 }
--- src/main/java/it/polimi/ingsw/cg23/view/ReadXml.java
+++ src/main/java/it/polimi/ingsw/cg23/view/ReadXml.java
@@ -14,9 +14,8 @@ import org.w3c.dom.Element;
  *
  */
 public class ReadXml {//http://www.mrwebmaster.it/java/xml-java-esempio-parsing-jaxp_7488_2.html
-	
 	String path="src/main/java/it/polimi/ingsw/cg23/view/ConfigurazionePartita.xml";
-	
+
 	/**
 	 * legge il file xml
 	 * @return bidimensional array with city info, null if there is some problem
@@ -28,34 +27,39 @@ public class ReadXml {//http://www.mrwebmaster.it/java/xml-java-esempio-parsing-
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();//inizializzato un nuovo documento
 			Document doc = dBuilder.parse(inputFile);//carica il documento dal file
 
-			Node rootnode = doc.getFirstChild();//recupera il primo nodo dell'xml (map)
+			/*Node rootnode = doc.getFirstChild();//recupera il primo nodo dell'xml (map)
 			Node zone = rootnode.getChildNodes().item(1);//primo elemento dei figli di map = secondo nodo xml (zone)
 			Node cities= zone.getChildNodes().item(3);//terzo elemento dei filgi di zone = quarto nodo xml (cities)
-			Node citty=cities.getChildNodes().item(1);// primo elemento dei figli di cities = qunto nodo xml (city)
+			Node citty=cities.getChildNodes().item(1);// primo elemento dei figli di cities = quinto nodo xml (city)
+			Node links=citty.getChildNodes().item(5);
+			*/
 			
-			int cityNodeNumber=(citty.getChildNodes().getLength()-1)/2; //numero di nodi di city
-						
-			NodeList citylist=doc.getElementsByTagName("city");//lista dei nodi che contengono "city"
-
-			int citynum=citylist.getLength();//numero di citta'
+			final int cityNodeNumber=cityNodeNumber(); //numero di nodi di city
+			final int citynum=cityNumber();//numero di citta'
 			String[][] city=new String[citynum][cityNodeNumber];//array per salvare le infromazioni delle citta'
 
-			for (int i=0; i<citylist.getLength(); i++){//scorre le citta' presenti nel file xml
+			NodeList citylist=doc.getElementsByTagName("city");//lista dei nodi che contengono "city"
+			NodeList zoneName=doc.getElementsByTagName("namez");//lista dei nodi che cntengono "namez" (nome della zona)
+			
+			for (int i=0; i<citynum; i++){//scorre le citta' presenti nel file xml
 				Node actualNode=citylist.item(i);//nodo attualmente in uso
 				Element actualElement=(Element) actualNode;//cast del nodo in elemento per poterlo usare
 
 				city[i][0]=actualElement.getElementsByTagName("name").item(0).getTextContent();//recupera il nome della città
 				city[i][1]=actualElement.getElementsByTagName("color").item(0).getTextContent();//recupera il colore della città
-				//city[i][2]=actualElement.getElementsByTagName("link").item(0).getTextContent();//recupera i link della città
+				city[i][2]=actualElement.getElementsByTagName("link").item(0).getTextContent();//recupera i link della città
 				city[i][3]=actualElement.getElementsByTagName("Id").item(0).getTextContent();//recupera l'id della città
 				city[i][4]=actualElement.getElementsByTagName("bonus").item(0).getTextContent();//recupera i bonus della città
+
+				Node actualZoneNode=zoneName.item(i/(citynum/zoneName.getLength()));//nodo zona delle citta'
+				city[i][5]=actualZoneNode.getTextContent();//recupera il tipo di citta' (costa, collina, mare)
 			}
 			return city;
 		} catch (Exception e) {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * calcola il numero di citta' nel file xml
 	 * @return the number of cities in the xml file, 0 if no city or error
@@ -73,7 +77,7 @@ public class ReadXml {//http://www.mrwebmaster.it/java/xml-java-esempio-parsing-
 			return 0;
 		}
 	}
-	
+
 	/**
 	 * calcola il numero di nodi (attributi) di citta'
 	 * @return the number of city nodes (attributes), 0 if no city or error
@@ -88,8 +92,8 @@ public class ReadXml {//http://www.mrwebmaster.it/java/xml-java-esempio-parsing-
 			Node zone = rootnode.getChildNodes().item(1);//primo elemento dei figli di map = secondo nodo xml (zone)
 			Node cities= zone.getChildNodes().item(3);//terzo elemento dei filgi di zone = quarto nodo xml (cities)
 			Node citty=cities.getChildNodes().item(1);// primo elemento dei figli di cities = qunto nodo xml (city)
-			
-			return (citty.getChildNodes().getLength()-1)/2; //numero di nodi di city
+
+			return (citty.getChildNodes().getLength()-1)/2+1; //numero di nodi di city, +1 perchè c'è da aggiungere zona
 		}
 		catch(Exception e) {
 			return 0;
