7036d9af507a9e73b99a35e72c44359e8ec2a947
Andrea <andrea.troianiello.94@gmail.com>
2016-05-02 21:09:28 +0200

    Completata la classe BusinessPermitTitle.

--- src/main/java/it/polimi/ingsw/cg23/model/components/BusinessPermitTitle.java
+++ src/main/java/it/polimi/ingsw/cg23/model/components/BusinessPermitTitle.java
@@ -1,5 +1,43 @@
 package it.polimi.ingsw.cg23.model.components;
 
+import java.util.ArrayList;
+import java.util.List;
+import it.polimi.ingsw.cg23.model.bonus.Bonus;
+
 public class BusinessPermitTitle {
-	//to do
+	
+	private final List<Character> citiesId;
+	private final List<Bonus> bonusTitle;
+
+	public BusinessPermitTitle(List<Character> citiesId){
+		this.citiesId=citiesId;
+		this.bonusTitle=new ArrayList<>();
+	}
+
+	/**
+	 * Returns the all id of cities, where you can build.
+	 * 
+	 * @return the citiesId
+	 */
+	public List<Character> getCitiesId() {
+		return citiesId;
+	}
+
+	/**
+	 * Returns all bonus of the title.
+	 * 
+	 * @return the bonusTitle
+	 */
+	public List<Bonus> getBonusTitle() {
+		return bonusTitle;
+	}
+	
+	/**
+	 * Adds a bonus in the title.
+	 * 
+	 * @param bonus
+	 */
+	public void addBonus(Bonus bonus){
+		this.bonusTitle.add(bonus);
+	}
 }
