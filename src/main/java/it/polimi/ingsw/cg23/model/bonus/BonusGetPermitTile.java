af0e6f1a7486e3ae65c731acb9eb904a92182cd8
doppiavi <vincenzovisco94@gmail.com>
2016-05-03 23:50:38 +0200

    solved an error

--- src/main/java/it/polimi/ingsw/cg23/model/bonus/BonusGetPermitTile.java
+++ src/main/java/it/polimi/ingsw/cg23/model/bonus/BonusGetPermitTile.java
@@ -1,13 +1,47 @@
 package it.polimi.ingsw.cg23.model.bonus;
-
+ 
 import it.polimi.ingsw.cg23.model.Player;
-
+import it.polimi.ingsw.cg23.model.components.BusinessPermitTile;
+import it.polimi.ingsw.cg23.model.components.RegionDeck;
+ 
 public class BonusGetPermitTile implements Bonus {
+	
+private final RegionDeck deck;			//wich region the player want to get the PermitTitle from
+private final int card; 				//wich PermitTitle the player choose from the showed ones 
+	
+	
+ 
+public BonusGetPermitTile(RegionDeck deck, int card) {
+	this.deck = deck;
+	this.card = card;
+}
 
-	@Override
-	public void giveBonus(Player player) {
-		// TODO Auto-generated method stub
+	
 
+/**
+ * @return the deck
+ */
+public RegionDeck getDeck() {
+	return deck;
+}
+
+
+/**
+ * @return the card
+ */
+public int getCard() {
+	return card;
+}
+
+/**
+ * give to the player the PermitTitle chosen and replace it 
+ * on the field with the one on the top of the region deck
+ */
+@Override
+public void giveBonus(Player player) {
+	BusinessPermitTile bonusPermit=this.deck.getBusinessPermitShowed().get(card);
+	player.getAvailableBusinessPermits().add(bonusPermit);   //add the choosen PermitTitle to the player collection
+	this.deck.changeShowedDeck();   						 //replace the PermitTitle chosen with the one in top of the deck
+ 
 	}
-
 }
