da3d8cb3b0cf6680de5b621797fa7da6f4e18fd9
viga94 <viga94_@hotmail.it>
2016-05-04 11:30:56 +0200

    sonar compliant update

--- src/main/java/it/polimi/ingsw/cg23/controller/Main.java
+++ src/main/java/it/polimi/ingsw/cg23/controller/Main.java
@@ -1,15 +1,8 @@
 package it.polimi.ingsw.cg23.controller;
 
-import it.polimi.ingsw.cg23.view.CliInterface;
-
 public class Main {
 
 	public static void main(String[] args) {
-		CliInterface cl=new CliInterface();
-		String[][]nopm=cl.startPartita();
-		//cl.printArray(nopm);
-		cl.savePartita(nopm);
-		cl.createMap(nopm, 4);
 		
 	}
 }
