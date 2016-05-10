057fd206e95bea10f523d146e79707cad1046862
viga94 <viga94_@hotmail.it>
2016-05-10 15:30:39 +0200

    aggiunti i vicini funzionanti

--- src/main/java/it/polimi/ingsw/cg23/controller/Setting.java
+++ src/main/java/it/polimi/ingsw/cg23/controller/Setting.java
@@ -12,7 +12,7 @@ public class Setting {
 
 	public void cityList(List<Region>regioni){
 
-		List<City>citta=new ArrayList<City>();
+		List<City>citta=new ArrayList<>();
 		for(int i=0; i<regioni.size(); i++){//ciclo che scorre le regioni
 			citta.addAll(regioni.get(i).getCities());//recupero la lista delle citta' di tutte le regioni			
 		}
@@ -22,24 +22,22 @@ public class Setting {
 	public void addNeighbours(List<City>citta){
 		String[][]cityInfo=cl.leggiXml("ConfigurazionePartita.xml");
 		for(int j=0; j<citta.size(); j++){//ciclo che scorre le citta
-
 			for(int i=0; i<cityInfo[j][2].length(); i++){//ciclo che scorre i vicini delle citta
-				//System.out.println(cityInfo[j][2].length());
-				String id=cityInfo[j][2].substring(i, i+1);//	CONTROLLARE
-				//System.out.println(id);
+				String id=cityInfo[j][2].substring(i, i+1);
+				//FUNZIONA
+				
+				//	CONTROLLARE->ERRORE
 				City vicina=getVicina(citta, id);
-
 				citta.get(j).addNeighbor(vicina);
-				//System.out.println("ok"+cityInfo[j][2]);
 			}
 		}
 	}
+	
 	public City getVicina(List<City>citta, String id){
 		for(int i=0; i<citta.size(); i++){
 			if(citta.get(i).getId()==id.charAt(0)){
 				return citta.get(i);
 			}
-			
 		}
 		return null;
 	}
