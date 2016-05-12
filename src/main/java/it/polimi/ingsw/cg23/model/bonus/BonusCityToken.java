5b73cf8dc0592d27a1a9afc31c90326b78f436e1
doppiavi <vincenzovisco94@gmail.com>
2016-05-12 13:55:14 +0200

    modified BonusCityToken

--- src/main/java/it/polimi/ingsw/cg23/model/bonus/BonusCityToken.java
+++ src/main/java/it/polimi/ingsw/cg23/model/bonus/BonusCityToken.java
@@ -8,13 +8,17 @@ import it.polimi.ingsw.cg23.model.Player;
 public class BonusCityToken implements Bonus {
 	
 	private final int number;				//how many times the player can run the bonus
-	private final City[] city;
-	
+	private final City[] city;				//the city the player chooses to run the bonus from
+	private final boolean[] bonusNobility;	//a list of boolean that show if the bonus in the city are referred to the nobilityBonus
+		
 	
 
 	public BonusCityToken(int number, City[] city) {
 		this.number = number;
 		this.city = city;
+		this.bonusNobility = new boolean[this.city.length];
+		for(int i=0; i<this.bonusNobility.length; i++)
+			this.bonusNobility[i] = false;
 	}
 
 	
@@ -41,9 +45,14 @@ public class BonusCityToken implements Bonus {
 	 */
 	@Override
 	public void giveBonus(Player player) {
-		for(int i=0; i<this.number; i++)					//iterate the city in the list
-			if(this.city[i].containsEmporium(player))		//control if the city contains an emporium
-				(this.city[i]).runBonusCity(player);		//if it does, give the player the bonus
+		for(int i=0; i<this.number; i++){											//iterate the city in the list
+			for(int j=0; j<this.city[i].getToken().size(); j++) 					//iterate the bonus in every city
+				if(this.city[i].getToken().get(j)=="BonusNobility") {				//if the city contains a nobilityBonus bonus
+					this.bonusNobility[i]=true;										//set as true the boolean referred to that city 
+					}
+			if(this.city[i].containsEmporium(player) && !this.bonusNobility[i])		//control if the city contains an emporium and if it doeasn't have bonusNobility in its bonuses
+				(this.city[i]).runBonusCity(player);								//if it does, give the player the bonus
+		}
 	}
 
 
