f2ce5924981f97cbdcf080a691d6f1da966853c7
Andrea <andrea.troianiello.94@gmail.com>
2016-05-13 18:21:31 +0200

    Update the BonusKing.

--- src/main/java/it/polimi/ingsw/cg23/model/bonus/BonusKing.java
+++ src/main/java/it/polimi/ingsw/cg23/model/bonus/BonusKing.java
@@ -1,30 +1,52 @@
 package it.polimi.ingsw.cg23.model.bonus;
 
+import java.util.List;
 import it.polimi.ingsw.cg23.model.Player;
 
 public class BonusKing {
-	private int bonusKing;
-	private int maxBonus;
+	private int index;								//The current bonus king.
+	private List<Integer> bonus;					//Set of all bonus king.
 	
-	public BonusKing(int length){
-		this.bonusKing=1;
-		this.maxBonus=length;
+	public BonusKing(List<Integer> bonus){
+		this.index=1;
+		this.bonus=bonus;
 	}
-	
 	
 	/**
+	 * Returns the value of current bonus king.
+	 * 
 	 * @return the bonusKing
 	 */
-	public int getBonusKing() {
-		return bonusKing;
-	}
-
-	public void increasePosition(){
-		this.bonusKing++;
+	public int getCurrentBonusKing() {
+		return bonus.get(index);
 	}
 	
+	/**
+	 * Returns the index of current bonus king.
+	 * 
+	 * @return the index
+	 */
+	public int getCurrentIndexBonusKing(){
+		return index;
+	}
+	
+	/**
+	 * Increases of one position the index of current bonus. 
+	 */
+	public void increasePosition(){
+		if(index<bonus.size()-1)					//The index can be at most the size of the set minus 1.
+			this.index++;
+	}
+	
+	/**
+	 * Runs the bonus with the current value and increases the index.
+	 * 
+	 * @param player
+	 */
 	public void runBonusKing(Player player){
-		
+		Bonus bonusKing=new BonusVictoryPoints(bonus.get(index));
+		bonusKing.giveBonus(player);
+		increasePosition();
 	}
 	
 }
