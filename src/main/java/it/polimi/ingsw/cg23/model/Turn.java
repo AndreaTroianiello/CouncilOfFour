7a941014957d7a1a28eeb8859a6128536c9bacef
Andrea <andrea.troianiello.94@gmail.com>
2016-05-15 21:00:57 +0200

    Modified Turn.

--- src/main/java/it/polimi/ingsw/cg23/model/Turn.java
+++ src/main/java/it/polimi/ingsw/cg23/model/Turn.java
@@ -7,11 +7,10 @@ import it.polimi.ingsw.cg23.model.components.Deck;
 
 public class Turn {
 	private final List<Player> players;											// the players of the game.
-	private Player currentPlayer;												//the current player.
-	private Player finalPlayer;													//the player who has built all emporiums available first. 
+	private int currentPlayer;												//the current player.
+	private int finalPlayer;													//the player who has built all emporiums available first.
 	private Action action;
 	private final Board board;
-	private boolean finalTurn;
 	private boolean mainAction;
 	private boolean secondAction;
 	
@@ -19,9 +18,9 @@ public class Turn {
 	public Turn(List<Player> players,Board board){
 		this.players=players;
 		this.board=board;
-		this.currentPlayer=null;
+		this.currentPlayer=0;
+		this.finalPlayer=-1;
 		this.action=null;
-		this.finalTurn=false;
 		this.mainAction=true;
 		this.secondAction=true;
 	}
@@ -32,7 +31,7 @@ public class Turn {
 	 * @return the currentPlayer
 	 */
 	public Player getCurrentPlayer() {
-		return currentPlayer;
+		return players.get(currentPlayer);
 	}
 
 	/**
@@ -41,14 +40,11 @@ public class Turn {
 	 * @return If true the game is finished and the first player of the list has built all emporiums available.
 	 */
 	public boolean changePlayer() {
-		if(currentPlayer!=null)
-			players.add(currentPlayer);											//Add the current player at the list.
-		if(finalTurn && finalPlayer.equals(players.get(0))){					//Control if the first player has finished the game.
-			currentPlayer=null;
-			return true;
+		if((currentPlayer+1)%players.size()!=finalPlayer){
+			currentPlayer=(currentPlayer+1)%players.size();
+			return false;
 		}
-		this.currentPlayer = players.remove(0);									//If false set the player as the current player.
-		return false;
+		return true;
 	}
 	
 	/**
@@ -64,7 +60,7 @@ public class Turn {
 	 * return finalTurn. It is true if a players has completed the emporiums.
 	 */
 	 public boolean isFinalTurn(){
-		 return finalTurn;
+		 return finalPlayer>=0;
 	 }
 	
 	/**
@@ -74,17 +70,18 @@ public class Turn {
 		Deck deck=board.getDeck();
 		if(deck.deckIsEmpty())
 			deck.changeDeck();
-		currentPlayer.addPoliticCard(deck.draw());
+		players.get(currentPlayer).addPoliticCard(deck.draw());
 	}
 	
 	/**
 	 * Runs the the action and control if the player has.
 	 */
 	public void runAction(){
-		action.runAction(currentPlayer, board);
-		if(!finalTurn && currentPlayer.isAvailableEmporiumEmpty()){
-			finalTurn=true;
-			currentPlayer.setVictoryPoints(currentPlayer.getVictoryPoints()+3);
+		Player player=players.get(currentPlayer);
+		action.runAction(player, board);
+		if(finalPlayer==-1 && player.isAvailableEmporiumEmpty()){
+			finalPlayer=currentPlayer;
+			player.setVictoryPoints(player.getVictoryPoints()+3);
 		}
 	}
 }
