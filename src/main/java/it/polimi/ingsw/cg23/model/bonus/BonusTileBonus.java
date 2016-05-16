7d8b8362d2b07fdc772a7115c072dce9db8f6f4a
doppiavi <vincenzovisco94@gmail.com>
2016-05-16 21:06:13 +0200

    modified BonusTileBonus

--- src/main/java/it/polimi/ingsw/cg23/model/bonus/BonusTileBonus.java
+++ src/main/java/it/polimi/ingsw/cg23/model/bonus/BonusTileBonus.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 import it.polimi.ingsw.cg23.model.Player;
 import it.polimi.ingsw.cg23.model.components.BusinessPermitTile;
+import it.polimi.ingsw.cg23.view.CliInterface;
 
 public class BonusTileBonus implements Bonus {
 	
 	private final List<BusinessPermitTile> businessPermitTiles;	//the list of PerimtTile the player choose to have the bonuses from
 	private final String name="TileBonus";
+	
+	private final CliInterface cl;
 
 	public BonusTileBonus(int number) {
 		this.businessPermitTiles = new ArrayList<>();
+		this.cl = new CliInterface();
 	}
 
 
@@ -37,6 +41,19 @@ public class BonusTileBonus implements Bonus {
 	 */
 	@Override
 	public void giveBonus(Player player) {
+		String c = (String)cl.writeReturnValue("Da che mazzo vuoi prendere il permesso?", null);
+		int card=(int)cl.writeReturnValue("Inserisci valore", null);
+		
+		if(c=="used"){
+			this.businessPermitTiles.add(player.getUsedBusinessPermit().get(card));
+		}
+		if(c=="available"){
+			this.businessPermitTiles.add(player.getAvailableBusinessPermits().get(card));
+		}
+		else{
+			System.out.println("La stringa immessa è incorretta");
+		}
+		
 		for(BusinessPermitTile bPT: this.businessPermitTiles){		//iterate the PermitsTile the player want to pick the bonuses from
 			for(Bonus bonus: bPT.getBonusTile()){					//for each PermitTite iterate the bonuses in it
 				bonus.giveBonus(player);							//for each bonus give it to the player
