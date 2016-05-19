c8254e58c626eb5b3ab329becab4478ddd8bac7f
viga94 <viga94_@hotmail.it>
2016-05-19 20:08:47 +0200

    add the class to red nobility track xml

---
+++ src/main/java/it/polimi/ingsw/cg23/view/ReadNobilityTrackXml.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.cg23.view;
+
+/**
+ * class to read the nobility track from xml
+ *
+ */
+public class ReadNobilityTrackXml {
+	//funzione che legge il numero di caselle
+	//funziona che legge i bonus e il numero di casella
+}
