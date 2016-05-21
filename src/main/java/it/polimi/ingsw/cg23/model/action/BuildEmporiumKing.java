2bafaf8cee02e0e6d2f53e178171a9ef5b630831
doppiavi <vincenzovisco94@gmail.com>
2016-05-21 12:23:51 +0200

    modified BuildEmporiumKing

--- src/main/java/it/polimi/ingsw/cg23/model/action/BuildEmporiumKing.java
+++ src/main/java/it/polimi/ingsw/cg23/model/action/BuildEmporiumKing.java
@@ -12,118 +12,121 @@ import it.polimi.ingsw.cg23.model.exception.NegativeNumberException;
 
 public class BuildEmporiumKing implements Action {
 	
-	private final List<PoliticCard> cards;
-	private final City destination;
 	private final boolean main;
-	List<PoliticCard> discardedCards = new ArrayList<>();
-		
+	private final List<PoliticCard> cards;
+	private List<PoliticCard> discardedCards = new ArrayList<>();
+	private final City destination;
 	
+	
+
 	public BuildEmporiumKing(List<PoliticCard> cards, City destination) {
+		this.main = true;
 		this.cards = cards;
 		this.destination = destination;
-		this.main = true;
 	}
 	
-	
-	
-	/**
-	 * @return the main
-	 */
+	@Override
 	public boolean isMain() {
 		return main;
 	}
 
-
-
-	/**
-	 * @return the cards
-	 */
-	public List<PoliticCard> getCards() {
-		return cards;
-	}
-
-
-
-	/**
-	 * @return the city
-	 */
-	public City getDestination() {
-		return destination;
-	}
-
-
-	/**
-	 * control how many cards match with the councillors, take the money from the player,
-	 * control if the path given is correct and build the emporium
-	 */
 	@Override
 	public void runAction(Player player, Board board) {
-		int[] howMany =  howManyMatch(board.getKing().getCouncil(), player, board);
-		int match = howMany[0];			//control how many match between cards and councillors there are
-		int jolly = howMany[1];
-		int moneyPaid = payCoins(match, player);						//make the player pay the relative amount of money
-		int steps = (int) board.getKing().getCity().minimumDistance(destination, new ArrayList<City>());					//set steps as the length of the list, and cast it to int
-		int coin = player.getRichness().getCoins();						//set coin as the current money of the player
-
-		if(moneyPaid!=-1){
+		int jolly = howManyJolly(board);															//control how many jolly there are
+		int match = jolly + howManyMatch(board, board.getKing().getCouncil());						//control how many match there are
+		int payMatch = payCoins(match, player);														//pay the amount of coins relative to the match
+		int steps = (int) board.getKing().getCity().minimumDistance(destination, new ArrayList<City>());		//control the distance between the king's city and the destination
+		int coin = player.getRichness().getCoins();													//control the richness of the player			
+		
+		if(payMatch!=-1){			
 			try {
-				coin = coin - steps*2 - jolly;									
-				player.getRichness().setCoins(coin);										//take from the player two coin for each steps the king moved
+				coin = coin - steps*2 - jolly;
+				player.getRichness().setCoins(coin);
 			} catch (NegativeNumberException e) {
-				System.out.println("The player doesn't have enough money");
+				System.out.println("The player doesn't have enough money!");
 				try {
-					player.getRichness().setCoins(coin+moneyPaid);						//if the player doesn't have enough money for the steps' payment, give him back the money previously paid 
-					return;
-				} catch (NegativeNumberException e1) {
+					player.getRichness().setCoins(coin+payMatch);
 					this.cards.addAll(discardedCards);
+				} catch (NegativeNumberException e1) {
 					e1.printStackTrace();
 				}
+			}
+			
+			if(player.getAvailableEmporium() != null){
+				buildEmporiumK(player, board, steps, jolly, payMatch);
+			}
+		}
+		
+		else{															//if the path isn't correct, give back the player the money previously paid
+			try {
+				player.getRichness().setCoins(player.getRichness().getCoins()+payMatch);
+			} catch (NegativeNumberException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			if(player.getAvailableEmporium() != null){					//if the player has available emporiums, build one in the city
-				try {
-					this.destination.buildEmporium(player.getAvailableEmporium());
-					board.getKing().setCity(destination);
-					board.getDeck().discardCars(discardedCards);
-				} catch (NegativeNumberException e) {
-					System.out.println("The player doesn't have available emporiums");
-					int currentCoin = player.getRichness().getCoins();
-					this.cards.addAll(discardedCards);
-					try {
-						player.getRichness().setCoins(currentCoin+steps*2+moneyPaid);	//if the player doesn't have available emporiums, give back the money previously paid
-					} catch (NegativeNumberException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					e.printStackTrace();					
-				}
+		}
+		
+	}
 
+	
+	/**
+	 * control how many jolly are there in the chosen politic cards
+	 * 
+	 * @param board
+	 * @return jolly
+	 */
+	public int howManyJolly(Board board){
+		int jolly = 0;
+		
+		for(PoliticCard card: cards)					//iterate the politic cards
+			if(card.isJolly()){							//if the card is a jolly
+				jolly = jolly + 1;						//update the counter
+				this.discardedCards.add(card);			//and add the card to the discardedCards
 			}
-
-			else{															//if the path isn't correct, give back the player the money previously paid
-				try {
-					player.getRichness().setCoins(player.getRichness().getCoins()+moneyPaid);
-				} catch (NegativeNumberException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+		this.cards.removeAll(discardedCards);			//remove all the jolly from the politic cards
+		
+		return jolly;
+	}
+	
+	
+	/**
+	 * control how many match between are there between 
+	 * the councillors and the politic cards
+	 * 
+	 * @param board
+	 * @param council
+	 * @return match
+	 */
+	public int howManyMatch(Board board, Council council){
+		int match = 0;
+		int councilLenght = council.getCouncillors().size();
+		
+		for(int i=0; i<councilLenght; i++){									//iterate the council
+			for(PoliticCard card: cards){									//iterate the politic cards
+				if(card.getColor().toString().equals(council.getCouncillors().get(i).toString())){
+					match = match + 1;										//if there is a match update the counter
+					this.discardedCards.add(card);							//add the card to the discarded cards
+					this.cards.remove(card);								//remove the card from the politic cards
+					break;													//and break the cycle
 				}
 			}
 		}
-
+		
+		return match;
 	}
-	
 
-	
-	/**
-	 * take the relative amount of money based on the number of match
-	 * @param cardNumber
-	 * @param chosenTile
-	 * @param player
-	 * @param board
-	 */
-	public int payCoins(int cardNumber, Player player){
+
+
+/**
+ * take the relative amount of money based on the number of match
+ * @param match
+ * @param chosenTile
+ * @param player
+ * @param board
+ */
+	public int payCoins(int match, Player player){
 		int coin = player.getRichness().getCoins();
-		switch(cardNumber){
+		switch(match){
 		case 1: 
 			if(tryPayment(player, coin, 10)!=-1)
 				return 10;
@@ -138,13 +141,14 @@ public class BuildEmporiumKing implements Action {
 			return -1;
 		case 4: 
 			return 0;
-			
+		
 		default: 
 			System.out.println("Your cards don't match any councillor");
 			return -1;
 			}
-		 
-		}
+	 }
+
+	
 	
 	/**
 	 * try to make the payment, and catch the exception if the 
@@ -165,65 +169,31 @@ public class BuildEmporiumKing implements Action {
 			return -1;
 		}
 	}
-	
+
 	/**
-	 * the method confront the color of the cards with the color of the councillors in the council
-	 * and return how many match are there 
-	 * @param council
-	 * @return cardNumber
-	 * @throws NegativeNumberException 
+	 * build the emporium in the destination
+	 * 
+	 * @param player
+	 * @param board
+	 * @param steps
+	 * @param payMatch
 	 */
-	public int[] howManyMatch(Council council, Player player, Board board){
-		
-		int councilLenght = council.getCouncillors().size();		//assign to councilLenght the value of the size of the chosen region's council
-		int jollyNumber = 0;										//the number of the jolly in the list
-		int cardNumber = 0;											//the number of cards that the color match with the councillor's colors
-		boolean match = false;										//control if there is a match
-		int[] result;												//an array with the card and the jolly number
-		result = new int[2];
-		
-		
-		for(PoliticCard card: this.cards){							//iterate the card
-			if(card.isJolly()){										//and control if there are jolly
-				cardNumber = cardNumber + 1;						//update the card counter
-				jollyNumber = jollyNumber + 1;						//update the jolly counter
-				discardedCards.add(card);
+	public void buildEmporiumK(Player player, Board board, int steps, int jolly, int payMatch){
+		try {
+			this.destination.buildEmporium(player.getAvailableEmporium());
+			board.getKing().setCity(destination);
+			board.getDeck().discardCars(discardedCards);
+		} catch (NegativeNumberException e) {
+			System.out.println("The player doesn't have available emporiums");
+			int currentCoin = player.getRichness().getCoins();
+			this.cards.addAll(discardedCards);
+			try {
+				player.getRichness().setCoins(currentCoin+steps*2+jolly+payMatch);	//if the player doesn't have available emporiums, give back the money previously paid
+			} catch (NegativeNumberException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
+			e.printStackTrace();					
 		}
-		cards.removeAll(discardedCards);
-		
-		for(int i=0; i<councilLenght; i++){							//iterate the council
-			for(PoliticCard card : this.cards){						//iterate the cards
-				if(card.getColor().toString().equals(council.getCouncillors().get(i).getColor().toString())){		
-					match = true;									//if there is a match set the boolean true 
-					discardedCards.add(card);
-				}   
-				if(match){											//if the match is true 
-					cardNumber = cardNumber + 1;					//update the counter
-					match = false;
-					break;											//and break the second for cycle
-				}
-			}
-		}
-		cards.removeAll(discardedCards);
-		
-		
-		result[0] = cardNumber;
-		result[1] = jollyNumber;
-		
-		return result;
 	}
-
-
-	
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "BuildEmporiumKing [cards=" + cards + ", destination=" + destination + "]";
-	}
-
-	
 }
