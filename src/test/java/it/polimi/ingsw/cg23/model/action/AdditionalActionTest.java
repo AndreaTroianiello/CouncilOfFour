22e76a18bc885790c0adaff3996d22650f4bf1d2
doppiavi <vincenzovisco94@gmail.com>
2016-05-24 14:25:30 +0200

    added AdditionalActionTest

---
+++ src/test/java/it/polimi/ingsw/cg23/model/action/AdditionalActionTest.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw.cg23.model.action;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import it.polimi.ingsw.cg23.model.Board;
+import it.polimi.ingsw.cg23.model.Player;
+import it.polimi.ingsw.cg23.model.components.NobilityTrack;
+
+public class AdditionalActionTest {
+
+	private Player player;
+	private Board board;
+	
+	@Before
+	public void setUp(){
+		player = new Player("player1",10,100,new NobilityTrack(3));
+		board = new Board(null, null, null, null, null);
+	}
+
+	@Test
+	public void ifAdditionalActionIsFalseItShouldBeChangedToTrue() {
+		if(!this.player.isAdditionalAction())
+			this.player.switchAdditionalAction();
+		AdditionalAction action = new AdditionalAction();
+		action.runAction(player, board);
+		assertEquals(true, player.isAdditionalAction());
+	}
+	
+	@Test
+	public void ifAdditionalActionIsTrueItShouldntBeChanged() {
+		if(!this.player.isAdditionalAction())
+			this.player.switchAdditionalAction();
+		AdditionalAction action = new AdditionalAction();
+		action.runAction(player, board);
+		assertEquals(true, player.isAdditionalAction());
+	}
+	
+	
+
+}
