e6bbdd465f2d45e5f165142d97b73735723f9a28
doppiavi <vincenzovisco94@gmail.com>
2016-05-27 17:35:19 +0200

    updated socket and finished a test

--- src/main/java/it/polimi/ingsw/cg23/client/socket/ClientOutHandler.java
+++ src/main/java/it/polimi/ingsw/cg23/client/socket/ClientOutHandler.java
@@ -4,8 +4,15 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
-
+import it.polimi.ingsw.cg23.model.action.AdditionalAction;
+import it.polimi.ingsw.cg23.model.action.BuildEmporiumKing;
+import it.polimi.ingsw.cg23.model.action.BuildEmporiumTile;
+import it.polimi.ingsw.cg23.model.action.BuyPermitTile;
+import it.polimi.ingsw.cg23.model.action.ChangeBusinessPermit;
+import it.polimi.ingsw.cg23.model.action.ElectCouncillor;
+import it.polimi.ingsw.cg23.model.action.ElectCouncillorAssistant;
 import it.polimi.ingsw.cg23.model.action.GameAction;
+import it.polimi.ingsw.cg23.model.action.HireAssistant;
 
 
 public class ClientOutHandler implements Runnable {
@@ -27,10 +34,55 @@ public class ClientOutHandler implements Runnable {
 			String inputLine = stdIn.nextLine();
 
 			GameAction action;
-			switch (inputLine) {
-			//DA FARE GLI INPUT DA INSERIRE PER OGNI AZIONE
-			default:
-				break;
+			try{
+				//DA COMPLETARE GLI ARGOMENTI PASSATI ALLE AZIONI
+				switch (inputLine) {
+				case "ADDITIONAL":
+					action = new AdditionalAction();
+					socketOut.writeObject(action);
+					socketOut.flush();
+					break;
+				case "BUILDKING":
+					action = new BuildEmporiumKing(null, null);
+					socketOut.writeObject(action);
+					socketOut.flush();
+					break;
+				case "BUILDTILE":
+					action = new BuildEmporiumTile(null, 0);
+					socketOut.writeObject(action);
+					socketOut.flush();
+					break;
+				case "BUYTILE":
+					action = new BuyPermitTile(null, null, null);
+					socketOut.writeObject(action);
+					socketOut.flush();
+					break;
+				case "CHANGE":
+					action = new ChangeBusinessPermit(0);
+					socketOut.writeObject(action);
+					socketOut.flush();
+					break;
+				case "ELECT":
+					action = new ElectCouncillor(null, null, true);
+					socketOut.writeObject(action);
+					socketOut.flush();
+					break;
+				case "ELECTASSISTANT":
+					action = new ElectCouncillorAssistant(null, null, true);
+					socketOut.writeObject(action);
+					socketOut.flush();
+					break;
+				case "HIRE":
+					action = new HireAssistant();
+					socketOut.writeObject(action);
+					socketOut.flush();
+					break;
+				default:
+					break;
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
 
 		}
--- src/test/java/it/polimi/ingsw/cg23/model/action/ChangeBusinessPermitTest.java
+++ src/test/java/it/polimi/ingsw/cg23/model/action/ChangeBusinessPermitTest.java
@@ -11,34 +11,65 @@ import org.junit.Test;
 import it.polimi.ingsw.cg23.model.Board;
 import it.polimi.ingsw.cg23.model.Player;
 import it.polimi.ingsw.cg23.model.Region;
+import it.polimi.ingsw.cg23.model.components.BusinessPermitTile;
 import it.polimi.ingsw.cg23.model.components.Deck;
 import it.polimi.ingsw.cg23.model.components.NobilityTrack;
 import it.polimi.ingsw.cg23.model.components.PoliticCard;
 import it.polimi.ingsw.cg23.model.components.RegionDeck;
+import it.polimi.ingsw.cg23.model.exception.NegativeNumberException;
 
 public class ChangeBusinessPermitTest {
 
 	private int number;
 	private Player player;
 	private Board board;
+	private List<BusinessPermitTile> deckCards = new ArrayList<>();
+	private List<BusinessPermitTile> showedDeck = new ArrayList<>();
+	List<Character> cities = new ArrayList<>();
 
 	@Before
 	public void setUp() throws Exception {
+		BusinessPermitTile card1 = new BusinessPermitTile(new ArrayList<>(), "si");
+		BusinessPermitTile card2 = new BusinessPermitTile(new ArrayList<>(), "no");
+		cities.add('I');
+		BusinessPermitTile card3 = new BusinessPermitTile(cities, "forse");
+		BusinessPermitTile card4 = new BusinessPermitTile(cities, "boh");
+		deckCards.add(card1);
+		deckCards.add(card2);
+		deckCards.add(card3);
+		deckCards.add(card4);
 		number = 0;
 		player = new Player("player1", 10, 10, new NobilityTrack(3));
 		List<Region> regions = new ArrayList<>();
 		RegionDeck deck = new RegionDeck(2);
-		//deck.
+		deck.setBusinessPermitTiles(deckCards);
+		showedDeck.addAll(deck.getShowedDeck());
 		Region region = new Region(null, 0, deck, null);
 		regions.add(region);
 		board = new Board(new Deck(new ArrayList<PoliticCard>()), regions, null, null, null);
 
 	}
 
+	/**
+	 * it tests if runAction() changes the showed cards when the player has enough assistants
+	 */
 	@Test
-	public void testRunAction() {
+	public void testRunActionShouldChangeTheShowedDeckWhenTiIsAllFine() {
 		ChangeBusinessPermit action = new ChangeBusinessPermit(number);
 		action.runAction(player, board);
+		assertNotEquals(showedDeck, board.getRegions().get(0).getDeck().getShowedDeck());
+	}
+	
+	/**
+	 * it tests if runAction() doesn't change the showed cards when the player hasn't enough assistants
+	 * @throws NegativeNumberException 
+	 */
+	@Test
+	public void testRunAction() throws NegativeNumberException {
+		ChangeBusinessPermit action = new ChangeBusinessPermit(number);
+		player.getAssistantsPool().setAssistants(0);
+		action.runAction(player, board);
+		assertEquals(showedDeck, board.getRegions().get(0).getDeck().getShowedDeck());
 	}
 
 
