d7047591f68f761402c50997a49617f1a62d291e
viga94 <viga94_@hotmail.it>
2016-06-07 14:25:20 +0200

    update gui

--- src/main/java/it/polimi/ingsw/cg23/gui/FrameMap.java
+++ src/main/java/it/polimi/ingsw/cg23/gui/FrameMap.java
@@ -16,6 +16,10 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JScrollPane;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
 public class FrameMap extends JFrame {
 
 	/**
@@ -23,18 +27,21 @@ public class FrameMap extends JFrame {
 	 */
 	private static final long serialVersionUID = 7022521494087312889L;
 	private JPanel contentPane;
-
+	private static Logger logger;
 	/**
 	 * Launch the application.
+	 * @param args
 	 */
 	public static void main(String[] args) {
+
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					FrameMap frame = new FrameMap();
 					frame.setVisible(true);
 				} catch (Exception e) {
-					e.printStackTrace();
+					logger.error("errore frame", e);				
 				}
 			}
 		});
@@ -44,21 +51,26 @@ public class FrameMap extends JFrame {
 	 * Create the frame.
 	 */
 	public FrameMap() {
+
+		//configurazione logger
+		logger = Logger.getLogger(FrameMap.class);
+		PropertyConfigurator.configure("src/main/resources/logger.properties");//carica la configurazione del logger
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1000, 1000);//dimensione finestra
-		
-		
+
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
-		
-		
+
+
 		contentPane.add(setBackground());//aggiunta dell'immagine di sfondo al jpanel
-		
+
 	}
 
 	public JScrollPane setBackground(){
@@ -67,10 +79,10 @@ public class FrameMap extends JFrame {
 		try {
 			image = ImageIO.read(new File("src/main/resources/BackgroundIMG.jpg"));
 		} catch (IOException e) {
-			
-			e.printStackTrace();
+
+			logger.error("impossibile caricare l'ìmmagine", e);
 		}
-		
+
 		//Create Image Label
 		JLabel label = new JLabel(new ImageIcon(image));
 		label.setBounds(0, 0, image.getWidth(), image.getHeight());
@@ -87,7 +99,8 @@ public class FrameMap extends JFrame {
 		JScrollPane scrollPanel = new JScrollPane(layeredPane);
 		scrollPanel.getVerticalScrollBar();//barra scorrimento verticale
 		scrollPanel.getHorizontalScrollBar();//barra scorrimento orizzontale
-		
+
 		return scrollPanel;
 	}
 }
+
---
+++ src/main/java/it/polimi/ingsw/cg23/gui/HelloFrame.java
@@ -0,0 +1,88 @@
+package it.polimi.ingsw.cg23.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ * fist gui class
+ *
+ */
+public class HelloFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5251939376496501206L;
+	private JPanel contentPane;
+	private static Logger logger;
+	
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//configurazione logger
+		logger = Logger.getLogger(HelloFrame.class);
+		PropertyConfigurator.configure("src/main/resources/logger.properties");//carica la configurazione del logger
+
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					HelloFrame frame = new HelloFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					logger.error("Impossibile creare il frame", e);
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HelloFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JPanel panel1 = new JPanel();
+		contentPane.add(panel1, BorderLayout.NORTH);
+
+		JLabel lblCof = new JLabel("Cof");
+		panel1.add(lblCof);
+
+		JPanel panel2 = new JPanel();
+		contentPane.add(panel2, BorderLayout.CENTER);
+
+		JButton btnRmi = new JButton("RMI");
+		btnRmi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel2.add(btnRmi);
+
+		JButton socket = new JButton("Socket");
+
+		socket.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel2.add(socket);
+	}
+
+}
+
