d0af8adeb35164562e4a1369978bdda69c0ac1e8
viga94 <viga94_@hotmail.it>
2016-06-14 10:46:10 +0200

    gui update

---
+++ src/main/java/it/polimi/ingsw/cg23/gui/Nuovo.java
@@ -0,0 +1,66 @@
+package it.polimi.ingsw.cg23.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JInternalFrame;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class Nuovo extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1328028752338623444L;
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 */
+	public Nuovo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new  GridBagLayout());
+		
+		grid();
+	}
+	
+	public void grid(){
+		contentPane.add(new JButton("1"));
+		contentPane.add(new JButton("2"));
+		contentPane.add(new JButton("3"));
+		contentPane.add(new JButton("4"));
+		contentPane.add(new JButton("5"));
+		contentPane.add(new JButton("6"));
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Nuovo frame = new Nuovo();
+					
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
