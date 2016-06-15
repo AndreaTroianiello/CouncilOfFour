9786eb92d5f660f585a5e4f09153b531b11666fd
viga94 <viga94_@hotmail.it>
2016-06-15 11:22:02 +0200

    add test

--- src/test/java/it/polimi/ingsw/cg23/view/MapSettingtest.java
+++ src/test/java/it/polimi/ingsw/cg23/view/MapSettingtest.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.cg23.view;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +20,7 @@ import it.polimi.ingsw.cg23.server.model.bonus.BonusCityToken;
 import it.polimi.ingsw.cg23.server.model.bonus.BonusCoin;
 import it.polimi.ingsw.cg23.server.model.components.BonusKing;
 import it.polimi.ingsw.cg23.server.model.components.BusinessPermitTile;
+import it.polimi.ingsw.cg23.server.model.components.Councillor;
 import it.polimi.ingsw.cg23.server.model.components.King;
 import it.polimi.ingsw.cg23.server.model.components.NobilityTrack;
 import it.polimi.ingsw.cg23.server.model.components.RegionDeck;
@@ -26,8 +28,8 @@ import it.polimi.ingsw.cg23.server.model.exception.NegativeNumberException;
 import it.polimi.ingsw.cg23.utility.MapSetting;
 
 public class MapSettingtest {
-	
-	MapSetting ms;
+
+	private MapSetting ms;
 	private Type type1,type2;
 	private List<Type> tipi;
 	private List<Region> reg;
@@ -37,49 +39,73 @@ public class MapSettingtest {
 	private King k;
 	private NobilityTrack nt;
 	private Board board;
-	
+	private List<BusinessPermitTile> bpt;
+
 	@Before
 	public void setUp() throws Exception {
+		bpt=new  ArrayList<>();
 		ms=new MapSetting();
 		tipi=new ArrayList<>();
 		reg=new ArrayList<>();
 		giocatori=new ArrayList<>();
-		
+
 		//Set up the bonus king
 		bonuses=new ArrayList<>();
 		bonuses.add(10);
 		bonuses.add(3);
 		bonuses.add(0);
 		BonusKing bonusKing=new BonusKing(bonuses);
-		
+
 		cities=new ArrayList<>();
 
 		//Set up the types.
 		type1=new Type("Gold",10,bonusKing);
 		type2=new Type("Silver",10,bonusKing);
-		
+
 		tipi.add(type1);//aggiungo i tipi alla lista
 		tipi.add(type2);
-		
+
 		//aggiungo regioni
 		reg.add(new Region("Region0",5,new RegionDeck(2),bonusKing));
 		reg.add(new Region("Region1",5,new RegionDeck(2),bonusKing));
 		reg.get(0).setBonusUnavailable();
+		
+		//aggiungo i consiglieri alle regioni
+		reg.get(0).getCouncil().getCouncillors().add(new Councillor(Color.blue));
+		reg.get(0).getCouncil().getCouncillors().add(new Councillor(Color.orange));
+		reg.get(0).getCouncil().getCouncillors().add(new Councillor(Color.gray));
+		reg.get(1).getCouncil().getCouncillors().add(new Councillor(Color.black));
+		reg.get(1).getCouncil().getCouncillors().add(new Councillor(Color.pink));
+		
+		//aggiungo le citta'
 		cities.add(new City('A', "Aosta", type1 , reg.get(0)));
 		cities.add(new City('B', "Bari", type2 , reg.get(0)));
 		cities.add(new City('C', "Crotone", type1 , reg.get(0)));
 		cities.add(new City('R', "Roma", type1 , reg.get(1)));
 		cities.add(new City('P', "Palermo", type2 , reg.get(1)));
 		cities.add(new City('M', "Milano", type2 , reg.get(1)));
-		
+
+		//vicini delle citta'
+		cities.get(0).addNeighbor(cities.get(1));
+		cities.get(1).addNeighbor(cities.get(2));
+		cities.get(2).addNeighbor(cities.get(3));
+		cities.get(3).addNeighbor(cities.get(4));
+		cities.get(4).addNeighbor(cities.get(5));
+		cities.get(5).addNeighbor(cities.get(0));
+
+		//bonus alle citta'
+		cities.get(0).addBonus(new BonusAdditionalAction());
+		cities.get(1).addBonus(new BonusAssistants());
+		cities.get(2).addBonus(new BonusCoin(10));
+
 		//re
 		k=new King(cities.get(0));
-		
+
 		//players
 		nt=new NobilityTrack(20);
 		giocatori.add(new Player("playerplayerplayer11", nt));
 		giocatori.add(new Player("player2", nt));
-		
+
 		//setto i giocatori
 		try {
 			giocatori.get(0).getAssistantsPool().setAssistants(10);
@@ -88,7 +114,7 @@ public class MapSettingtest {
 		} catch (NegativeNumberException e) {
 			e.printStackTrace();
 		}
-		
+
 		//creazione carte costruzione
 		List<Character> c1=new ArrayList<>();
 		c1.add('A');
@@ -106,7 +132,8 @@ public class MapSettingtest {
 		bl1.add(b1);
 		bl1.add(b2);
 		reg.get(0).getDeck().setBusinessPermitTiles(bl1);
-		
+		bpt.addAll(bl1);
+
 		//creazione carte costruzione
 		List<Character> c3=new ArrayList<>();
 		c3.add('R');
@@ -125,7 +152,8 @@ public class MapSettingtest {
 		bl2.add(b3);
 		bl2.add(b4);
 		reg.get(1).getDeck().setBusinessPermitTiles(bl2);
-		
+		bpt.addAll(bl2);
+
 		board=new Board(null, reg, tipi, nt, k, bonusKing);
 		board.addPlayer(giocatori.get(0));
 		board.addPlayer(giocatori.get(1));
@@ -134,7 +162,7 @@ public class MapSettingtest {
 	@Test
 	public void getCityfromRegionTest() {
 		List<City> citta=ms.getCityfromRegion(reg);
-		
+
 		assertEquals(citta.size(), 6);
 		for(int i=0; i<cities.size(); i++){//puo' essere che non tutti i parametri siano settati
 			assertEquals(citta.get(i).getName(), cities.get(i).getName());
@@ -146,6 +174,85 @@ public class MapSettingtest {
 			assertEquals(citta.get(i).getType(), cities.get(i).getType());
 		}
 	}
+
+	@Test
+	public void cityBonusTest(){
+		assertEquals(ms.cityBonus(cities.get(0)), "AdditionalAction");
+		assertEquals(ms.cityBonus(cities.get(1)), "0Assistants");
+		assertEquals(ms.cityBonus(cities.get(2)), "10Coin");
+
+	}
+
+	@Test
+	public void addSpaceTest(){
+		String prova="prova";
+		int space=10;
+		assertEquals(ms.addSpace(prova, space).length(), space);
+		assertEquals(ms.addSpace("provaprova", space).length(), space);
+		assertEquals(ms.addSpace(34, space).length(), space);
+	}
+
+	@Test
+	public void printNameTest(){
+		String nome=ms.printName(reg, 50);
+		assertTrue(nome.contains(reg.get(0).getName().toUpperCase()));
+		assertTrue(nome.contains(reg.get(1).getName().toUpperCase()));
+		assertEquals(nome.length(), 50*reg.size());
+	}
+
+	@Test
+	public void createCostuctionCardTest(){
+		String nome=ms.createCostructionShowed(reg, 50);
+		assertTrue(nome.contains(reg.get(0).getName()));
+		assertTrue(nome.contains(reg.get(1).getName()));
+		assertTrue(nome.contains("Carte Costruzione"));
+		assertEquals(nome.length(), 50*6*4+6);
+		for(int i=0; i<bpt.size(); i++){
+			assertTrue(nome.contains(bpt.get(i).getCitiesId().toString()));
+			assertTrue(nome.contains(bpt.get(i).getBonusTile().get(0).getName()));
+		}
+	}
+
+	@Test
+	public void neightbourTest(){
+		for(int i=0; i<cities.size(); i++){
+			String nome=ms.getNeighbourID(cities.get(i));
+			assertTrue(nome.contains("Vicini:"));
+			assertTrue(nome.contains(""+cities.get(i).getNeighbors().get(0).getId()));
+		}
+	}
 	
+	@Test
+	public void addMinusTest(){
+		String nome=ms.addMinus(20);
+		assertEquals(nome.length(), 20);
+		assertNotEquals(nome, "");
+		assertNotEquals(nome, null);
+		assertTrue(nome.contains("--"));
+	}
+	
+	@Test
+	public void councillorsTest(){
+		for(int i=0; i<reg.size(); i++){
+			String nome=ms.councillors(reg);
+			assertTrue(nome.contains("Consiglieri "+reg.get(i).getName()));
+			assertNotEquals(nome, "");
+			assertNotEquals(nome, null);
+			
+		}
+	}
+	
+	@Test
+	public void bonusCouncilKingTypetest(){
+		String nome=ms.bonusCouncilKingType(board);
+		assertEquals(nome.length(), 50*3);
+		assertTrue(nome.contains("Consiglieri del re:"));
+		assertTrue(nome.contains("Bonus king avaiable:"));
+		assertTrue(nome.contains("Avaiable type:"));
+		assertNotEquals(nome, "");
+		assertNotEquals(nome, null);
+		assertTrue(nome.contains(tipi.get(0).getName()));
+		assertTrue(nome.contains(tipi.get(1).getName()));
+	}
 
 }
