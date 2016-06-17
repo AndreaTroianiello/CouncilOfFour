899226e19770f1324a8ad90bace4c2435ee83fbf
Andrea <andrea.troianiello.94@gmail.com>
2016-06-17 21:50:37 +0200

    Update HomeFrame.

--- src/main/java/it/polimi/ingsw/cg23/gui/HomeFrame.java
+++ src/main/java/it/polimi/ingsw/cg23/gui/HomeFrame.java
@@ -25,20 +25,29 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class HomeFrame extends JFrame {
 	
 	private static final long serialVersionUID = -7250417194617661777L;
+	private ControllerGUI controller;
+	
 	private JButton buttonSocket;	//button1
     private JButton buttonRMI;      //button2
     private JLabel labelBackground; //label1
     private JLabel labelAddress;	//label2
-    private JPanel panel;			//panel1
+    private JPanel panelConnaction;			//panel1
     private JTextField fieldAddress;//field1
-	private Logger logger;
-	private ControllerGUI controller;
+	private static Logger logger;
+	
+	private JPanel panelLoggin;
+	private JLabel labelInfo1;
+	private JLabel labelLoggin;
+	private JButton buttonLoggin;
+	private JLabel labelInfo2;
+	private JTextField fieldLoggin;
 
 	/**
 	 * Launch the application.
@@ -50,7 +59,7 @@ public class HomeFrame extends JFrame {
 					HomeFrame frame = new HomeFrame();
 					frame.setVisible(true);
 				} catch (Exception e) {
-					e.printStackTrace();
+					logger.error(e);
 				}
 			}
 		});
@@ -68,8 +77,17 @@ public class HomeFrame extends JFrame {
 
 	
 	 private void initComponents() {
+		 	panelLoggin=new JPanel();
+			labelInfo1=new JLabel();
+			labelLoggin=new JLabel();
+			buttonLoggin=new JButton();
+			labelInfo2=new JLabel();
+			fieldLoggin=new JTextField();
 
-	        panel = new JPanel();
+		 
+		 	//panel1
+	        panelConnaction = new JPanel();
+	        panelLoggin = new JPanel();
 	        labelAddress = new JLabel();
 	        fieldAddress = new JTextField();
 	        buttonSocket = new JButton();
@@ -86,7 +104,70 @@ public class HomeFrame extends JFrame {
 	        setResizable(false);
 	        getContentPane().setLayout(null);
 
-	        panel.setOpaque(false);
+	        //Panel 2
+	        panelLoggin.setOpaque(false);
+	        panelLoggin.setVisible(false);
+	        labelInfo1.setForeground(new Color(255, 215, 0));
+	        labelInfo1.setText("Connection created");
+
+	        labelLoggin.setForeground(new java.awt.Color(255, 215, 0));
+	        labelLoggin.setText("Insert your username:");
+
+	        buttonLoggin.setText("Loggin");
+	        buttonLoggin.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent evt) {
+	                buttonLogginActionPerformed(evt);
+	            }
+	        });
+
+	        labelInfo2.setForeground(new java.awt.Color(255, 215, 0));
+	        labelInfo2.setText("");
+
+	        GroupLayout panelLogginLayout = new javax.swing.GroupLayout(panelLoggin);
+	        panelLoggin.setLayout(panelLogginLayout);
+	        panelLogginLayout.setHorizontalGroup(
+	            panelLogginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	            .addGroup(panelLogginLayout.createSequentialGroup()
+	                .addGap(23, 23, 23)
+	                .addComponent(labelLoggin)
+	                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+	                .addGroup(panelLogginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	                    .addGroup(panelLogginLayout.createSequentialGroup()
+	                        .addComponent(labelInfo1)
+	                        .addGap(0, 159, Short.MAX_VALUE))
+	                    .addComponent(fieldLoggin))
+	                .addContainerGap())
+	            .addGroup(panelLogginLayout.createSequentialGroup()
+	                .addGap(152, 152, 152)
+	                .addGroup(panelLogginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	                    .addGroup(panelLogginLayout.createSequentialGroup()
+	                        .addGap(10, 10, 10)
+	                        .addComponent(labelInfo2))
+	                    .addComponent(buttonLoggin))
+	                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+	        );
+	        panelLogginLayout.setVerticalGroup(
+	        		panelLogginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	            .addGroup(panelLogginLayout.createSequentialGroup()
+	                .addGap(88, 88, 88)
+	                .addComponent(labelInfo1)
+	                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+	                .addGroup(panelLogginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+	                    .addComponent(labelLoggin)
+	                    .addComponent(fieldLoggin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+	                .addGap(18, 18, 18)
+	                .addComponent(buttonLoggin)
+	                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+	                .addComponent(labelInfo2)
+	                .addContainerGap(26, Short.MAX_VALUE))
+	        );
+
+	        getContentPane().add(panelLoggin);
+	        panelLoggin.setBounds(-10, 230, 400, 220);
+	        
+	        
+	        //Panel 1
+	        panelConnaction.setOpaque(false);
 
 	        labelAddress.setForeground(new Color(255, 215, 0));
 	        labelAddress.setText("IP Address:");
@@ -105,8 +186,8 @@ public class HomeFrame extends JFrame {
 	            }
 	        });
 
-	        GroupLayout panelLayout = new GroupLayout(panel);
-	        panel.setLayout(panelLayout);
+	        GroupLayout panelLayout = new GroupLayout(panelConnaction);
+	        panelConnaction.setLayout(panelLayout);
 	        panelLayout.setHorizontalGroup(
 	        	panelLayout.createParallelGroup(Alignment.LEADING)
 	            .addGroup(Alignment.TRAILING, panelLayout.createSequentialGroup()
@@ -135,8 +216,8 @@ public class HomeFrame extends JFrame {
 	                .addContainerGap(64, Short.MAX_VALUE))
 	        );
 
-	        getContentPane().add(panel);
-	        panel.setBounds(0, 230, 400, 230);
+	        getContentPane().add(panelConnaction);
+	        panelConnaction.setBounds(0, 230, 400, 230);
 	        
 	        try {
 				BufferedImage image = ImageIO.read(new File("src/main/resources/images/Home-CouncilOfFour.jpg"));
@@ -154,8 +235,11 @@ public class HomeFrame extends JFrame {
 		 try {
 				ClientSocket clientSocket=new ClientSocket();
 				clientSocket.startClient(controller,fieldAddress.getText());
+				panelConnaction.setVisible(false);
+				panelLoggin.setVisible(true);
 			} catch (IOException e) {
 				logger.error(e);
+				JOptionPane.showMessageDialog(null, "Unable to contact server with a SOCKET connection.", "ERROR", JOptionPane.WARNING_MESSAGE);
 			}
 	 }
 	 
@@ -163,8 +247,16 @@ public class HomeFrame extends JFrame {
 		 try {
 				ClientRMI clientRMI=new ClientRMI();
 				clientRMI.startClient(controller,fieldAddress.getText());
+				panelConnaction.setVisible(false);
+				panelLoggin.setVisible(true);
 			} catch (IOException | NotBoundException e) {
 				logger.error(e);
+				JOptionPane.showMessageDialog(null, "Unable to contact server with a RMI connection.", "ERROR", JOptionPane.WARNING_MESSAGE);
 		}
-	 } 
+	 }
+	 
+	 private void buttonLogginActionPerformed(ActionEvent evt) {
+			// TODO Auto-generated method stub
+			
+	}
 }
