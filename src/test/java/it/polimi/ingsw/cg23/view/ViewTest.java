368d8990aa35c6d6bbbea714886ee9d94f12ea8d
Andrea <andrea.troianiello.94@gmail.com>
2016-07-03 23:22:45 +0200

    Minor updates

--- src/test/java/it/polimi/ingsw/cg23/view/ViewTest.java
+++ src/test/java/it/polimi/ingsw/cg23/view/ViewTest.java
@@ -9,6 +9,11 @@ import it.polimi.ingsw.cg23.server.view.View;
 
 public class ViewTest {
 	private View view;
+	
+	/**
+	 * Initializes the view with anonymous class.
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		class TestView extends View{
@@ -16,6 +21,9 @@ public class ViewTest {
 		view=new TestView();
 	}
 
+	/**
+	 * Test getSuspended() and setSuspended() method.
+	 */
 	@Test
 	public void testSuspended() {
 		assertFalse(view.getSuspended());
@@ -23,6 +31,9 @@ public class ViewTest {
 		assertTrue(view.getSuspended());
 	}
 
+	/**
+	 * Test getLogger() and setLogger() method.
+	 */
 	@Test
 	public void testLogger() {
 		assertNotNull(view.getLogger());
